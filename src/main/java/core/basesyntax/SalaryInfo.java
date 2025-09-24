2e5a2d38e1a967241bbbbd0c6253c1f75f964cba
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-09-24 15:31:14 +0200

    Add fixes

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,27 +4,33 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern(DATE_PATTERN);
+    private static final String SPLIT_REGEX = "\\s+";
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int MONEY_PER_HOUR_INDEX = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-        LocalDate from = LocalDate.parse(dateFrom, formatter);
-        LocalDate to = LocalDate.parse(dateTo, formatter);
+        LocalDate from = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, DATE_FORMATTER);
         int[] totals = new int[names.length];
 
-        for (String d : data) {
+        for (String dataRecord : data) {
 
-            if (d == null || d.isBlank()) {
+            if (dataRecord == null || dataRecord.isBlank()) {
                 continue;
             }
-            String[] parts = d.trim().split("\\s+");
-            String date = parts[0];
-            String nameOfEmployee = parts[1];
-            int hours = Integer.parseInt(parts[2]);
-            int moneyPerHour = Integer.parseInt(parts[3]);
+            String[] parts = dataRecord.trim().split(SPLIT_REGEX);
+            String date = parts[DATE_INDEX];
+            String nameOfEmployee = parts[NAME_INDEX];
+            int hours = Integer.parseInt(parts[HOURS_INDEX]);
+            int moneyPerHour = Integer.parseInt(parts[MONEY_PER_HOUR_INDEX]);
             int earned = hours * moneyPerHour;
 
-            LocalDate current = LocalDate.parse(date, formatter);
+            LocalDate current = LocalDate.parse(date, DATE_FORMATTER);
 
             if (current.isBefore(from) || current.isAfter(to)) {
                 continue;
